6eaa0c430582b54a02aa8f51dbd74085e2d78811
manihatn <nm525@cl.cam.ac.uk>
2015-11-09 12:13:29 +0000

    updated delay parameters

--- link.h
+++ link.h
@@ -3,11 +3,17 @@
 class link
 {
 	public:
-		float bandwidth;
-		float transmission_delay;
-		float propagation_latency;
-		float loss_rate;
+		double bandwidth;
+		double transmission_delay;
+		double propagation_latency;
+		double loss_rate;
 		host *source;
 	        host *destination;	
 		
+                //void link_properties(); // setting the transmission delay, propagation delay, bandwidth etc. 
+		link* attach( host server, host client, double bandwidth, double length);
+		void disp_statistics();
+
+
+
 };
--- main.cc
+++ main.cc
@@ -5,6 +5,8 @@
 using namespace std;
 
 //mypacket= new packet(2,"murugan");
+const double c=2*10e8;
+
 
 int main()
 {
@@ -23,6 +25,36 @@ host client;
 client.set_host(1,"client",4);
 client.recv_pkt(mypacket);
 client.display_recv_pkt(mypacket);
+//link simplelink;
+//simplelink.properties(); // setting the transmission delay, propagation delay, bandwidth etc. 
+//simplelink.attach(server, client);
+//simplelink.disp_statistics();
+
+// Transmission delay = flow_size/link_capacity
+// Propagation delay = time taken to send the data via the link 
+
+// This is how the link function should look like
+
+
+// link(double length, double capacity, host server, host client ) 
+
+
+double fs=10e3;
+double cap=10e9;
+double len= 5 ;// in meters, this needs to be modified to be generic. 
+
+double td = fs/cap;
+double tp = len/c;
+        cout<<"Transmission delay (in seconds): "<<td << "\n";
+	cout<<"Propagation delay (in seconds): "<<tp << "\n";
+	cout<<"Total delay (in seconds): "<<td+tp << "\n";
+
+
+// Crete a simple link 
+// 
+// create syn , fin and ack style packets 
+// Use the parameters of congestion control algorithms to initialize 
+// 
 
 
 return 0;
